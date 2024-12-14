bd7dd4af0980649e5615f1bd36eadd2f7c4a4585
Ali-404 <abdellali.aitaddi@gmail.com>
2024-12-15 00:36:22 +0100

    Adding mongodb, Complete auth

--- notesProject/Auth/models.py
+++ notesProject/Auth/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+# from django.db import models
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from mongo import db,client
 
-# Create your models here.
+from datetime import datetime
+
+from django.contrib.auth.hashers import make_password
+
+
+def get_user_by_email(email: str):
+    return db["users"].find_one({
+        "email": email,
+    })
+    
+    
+
+
+
+def check_password(password):
+    try:
+        validate_password(password)  # This checks the password
+        return True,"Password is valid."
+    except ValidationError as e:
+        return False,f"Password is invalid: {', '.join(e.messages)}"
+    
+def create_user(email:str, password:str):
+    
+    hashed_pass = make_password(password)
+    
+    return db["users"].insert_one({
+        "email": email,
+        "password": hashed_pass,
+        "createdAt": datetime.now()
+    })
+    
--- notesProject/Auth/views.py
+++ notesProject/Auth/views.py
@@ -1,12 +1,72 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from .models import get_user_by_email,check_password,create_user
 # Create your views here.
 
 
-def LoginPage(request):
+def Login(request: HttpRequest):
+    pass 
+
+
+def LoginPage(request: HttpRequest):
+    if (request.method == "POST"):
+        Login(request)
+        
     return render(request, "Login.html")
 
 
 
-def RegisterPage(request):
-    return render(request, "Register.html")
+
+def Register(request:HttpRequest):
+    error = ""
+    
+    
+    # validate email
+    email = request.POST.get("register_email")
+    
+    if (get_user_by_email(email)):
+        error = "Email already taken! "
+        return error    
+    
+    
+    
+    password = request.POST.get("register_password")
+    
+    valid,msg = check_password(password)
+    if (not valid):
+        error = msg
+        return error
+    
+    
+    password_conferm = request.POST.get("register_password_confirm")
+    
+    if (password != password_conferm):
+        error  = "Passwords do not match! "
+        return error
+    
+    
+    # create user
+    user = create_user(email, password)
+    
+    if (user):
+        error = ""
+    else:
+        error = "There is an error !"
+    
+    return error
+
+
+def RegisterPage(request:HttpRequest):
+    
+    
+    error = ""
+    if (request.method == "POST"):
+        error = Register(request)
+        if (not error or error == ""):
+            return redirect("Login")
+    return render(request, "Register.html", {
+        "error": error
+    })
+
+
+
