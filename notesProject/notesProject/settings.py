465975f1d2a58a894bcd563bd0452b662b5f0a90
Ali-404 <abdellali.aitaddi@gmail.com>
2025-01-13 17:27:58 +0100

    back to prev commit

--- notesProject/notesProject/settings.py
+++ notesProject/notesProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-idw=byr*st^o)jf_7$lf-2zsctd51za_k(_+hy@w!%(tb*(oo*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
